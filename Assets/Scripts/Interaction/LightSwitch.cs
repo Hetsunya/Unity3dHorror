7a97a90ccfe641f48585c27e5a50ec7a96082ce6
Fanatsio <68788658+Fanatsio@users.noreply.github.com>
2023-07-06 17:42:17 +0500

    Animation

---
+++ Assets/Scripts/Interaction/LightSwitch.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour, IInteractable
+{
+    public Light m_Light;
+    public bool isOn;
+
+    void Start()
+    {
+        m_Light.enabled = isOn;
+    }
+
+    public string GetDescription()
+    {
+        if (isOn) return "Press [LMB] to pull the lever.";
+        return "Press [LMB] to pull the lever.";
+    }
+
+    public void Interact()
+    {
+        isOn = !isOn;
+        m_Light.enabled = isOn;
+    }
+}
