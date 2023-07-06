5e02b29bc6af796f1c993152de03403a6bef1f63
Hetsunya <99246859+Hetsunya@users.noreply.github.com>
2023-07-06 17:06:25 +0500

    Timer
    Сделал таймер для включения света и ограничил срабатывание скрипта

--- Assets/Scripts/01/LightSwitch.cs
+++ Assets/Scripts/01/LightSwitch.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class LightSwitch : MonoBehaviour
 {
-    public float interactionDistance = 1f; // Максимальное расстояние для взаимодействия
-    private bool lightsOn = true;
+    public float interactionDistance = 2f; // Максимальное расстояние для взаимодействия
+    public float delayTime = 5f; // Задержка перед отключением света
+    private bool lightsOn = true; // Изначально свет включен
+    private bool interacted = false; // Флаг для отслеживания взаимодействия
+    private float timer = 0f; // Таймер для отслеживания задержки
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (!interacted && Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
             Camera camera = GameObject.Find("Camera").GetComponent<Camera>();
@@ -19,8 +22,19 @@ public class LightSwitch : MonoBehaviour
             {
                 if (hit.collider.CompareTag("Light"))
                 {
-                    ToggleLights();
+                    interacted = true; // Установка флага взаимодействия
+                    timer = delayTime; // Задаем значение таймера
+                    ToggleLights(); // Выключаем свет
                 }
+            }
+        }
+
+        if (interacted && timer > 0f)
+        {
+            timer -= Time.deltaTime; // Уменьшаем таймер
+            if (timer <= 0f)
+            {
+                ToggleLights(); // Включаем свет после задержки
             }
         }
     }
@@ -40,16 +54,13 @@ public class LightSwitch : MonoBehaviour
     }
 }
 
-
-
 // using System.Collections;
 // using System.Collections.Generic;
 // using UnityEngine;
 
 // public class LightSwitch : MonoBehaviour
 // {
-//     public bool dist;
-//     public Camera mainCam;
+//     public float interactionDistance = 1f; // Максимальное расстояние для взаимодействия
 //     private bool lightsOn = true;
 
 //     private void Update()
@@ -60,7 +71,7 @@ public class LightSwitch : MonoBehaviour
 //             Camera camera = GameObject.Find("Camera").GetComponent<Camera>();
 //             Ray ray = camera.ScreenPointToRay(Input.mousePosition);
 
-//             if (Physics.Raycast(ray, out hit))
+//             if (Physics.Raycast(ray, out hit, interactionDistance))
 //             {
 //                 if (hit.collider.CompareTag("Light"))
 //                 {
@@ -72,7 +83,6 @@ public class LightSwitch : MonoBehaviour
 
 //     private void ToggleLights()
 //     {
-//         print("Работает");
 //         lightsOn = !lightsOn;
 //         GameObject[] lights = GameObject.FindGameObjectsWithTag("Light");
 //         foreach (GameObject lightObj in lights)
