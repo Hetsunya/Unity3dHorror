8d14f64c0b6406044581aa15276b343275dd8fa6
Hetsunya <99246859+Hetsunya@users.noreply.github.com>
2023-07-06 17:15:57 +0500

    Finaly
    Уххх

--- Assets/Scripts/01/LightSwitch.cs
+++ Assets/Scripts/01/LightSwitch.cs
@@ -2,10 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+            RaycastHit hit;
+            Camera camera = GameObject.Find("Camera").GetComponent<Camera>();
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+*/
+
 public class LightSwitch : MonoBehaviour
 {
     public float interactionDistance = 2f; // Максимальное расстояние для взаимодействия
     public float delayTime = 5f; // Задержка перед отключением света
+    public AudioSource audioSource; // Ссылка на компонент AudioSource для проигрывания аудио
+    public AudioClip audioClip; // Аудиоклип для проигрывания
     private bool lightsOn = true; // Изначально свет включен
     private bool interacted = false; // Флаг для отслеживания взаимодействия
     private float timer = 0f; // Таймер для отслеживания задержки
@@ -25,6 +33,7 @@ public class LightSwitch : MonoBehaviour
                     interacted = true; // Установка флага взаимодействия
                     timer = delayTime; // Задаем значение таймера
                     ToggleLights(); // Выключаем свет
+                    PlayAudio(); // Проигрываем аудио
                 }
             }
         }
@@ -35,6 +44,8 @@ public class LightSwitch : MonoBehaviour
             if (timer <= 0f)
             {
                 ToggleLights(); // Включаем свет после задержки
+                StopAudio(); // Прекращаем проигрывание аудио
+
             }
         }
     }
@@ -52,46 +63,20 @@ public class LightSwitch : MonoBehaviour
             }
         }
     }
+
+    private void PlayAudio()
+    {
+        if (audioSource != null && audioClip != null)
+        {
+            audioSource.PlayOneShot(audioClip);
+        }
+    }
+
+    private void StopAudio()
+    {
+        if (audioSource != null)
+        {
+            audioSource.Stop();
+        }
+    }
 }
-
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// public class LightSwitch : MonoBehaviour
-// {
-//     public float interactionDistance = 1f; // Максимальное расстояние для взаимодействия
-//     private bool lightsOn = true;
-
-//     private void Update()
-//     {
-//         if (Input.GetMouseButtonDown(0))
-//         {
-//             RaycastHit hit;
-//             Camera camera = GameObject.Find("Camera").GetComponent<Camera>();
-//             Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-
-//             if (Physics.Raycast(ray, out hit, interactionDistance))
-//             {
-//                 if (hit.collider.CompareTag("Light"))
-//                 {
-//                     ToggleLights();
-//                 }
-//             }
-//         }
-//     }
-
-//     private void ToggleLights()
-//     {
-//         lightsOn = !lightsOn;
-//         GameObject[] lights = GameObject.FindGameObjectsWithTag("Light");
-//         foreach (GameObject lightObj in lights)
-//         {
-//             Light lightComponent = lightObj.GetComponent<Light>();
-//             if (lightComponent != null)
-//             {
-//                 lightComponent.enabled = lightsOn;
-//             }
-//         }
-//     }
-// }
